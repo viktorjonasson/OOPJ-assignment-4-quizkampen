d48f83d0e7e3114bed244ad30260553813417faa
Vasahlin <Vasahlin@gmail.com>
2024-11-20 11:11:24 +0100

    Started working on a color changing feature for buttons

--- client/GUI.java
+++ client/GUI.java
@@ -1,8 +1,15 @@
+import javax.swing.*;
 import java.awt.*;
 
 public class GUI {
-    public void changeColor() {
+    public void changeColor(JButton buttonToChange, boolean correctAnswer) {
         Color correctColor = new Color(83, 214, 49, 203);
-        Color incorrectColor = new Color(83, 214, 49, 203);
+        Color incorrectColor = new Color(225, 52, 123, 203);
+        if (correctAnswer) {
+            buttonToChange.setBackground(correctColor);
+        } else {
+            buttonToChange.setBackground(incorrectColor);
+        }
     }
+
 }
