0790882f0bfdf2ecd4364a354627960a4d8600a8
Vasahlin <Vasahlin@gmail.com>
2024-11-20 10:56:03 +0100

    Started working on a color changing feature for buttons

---
+++ client/GUI.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class GUI {
+    public void changeColor() {
+        Color correctColor = new Color(83, 214, 49, 203);
+        Color incorrectColor = new Color(83, 214, 49, 203);
+    }
+}
