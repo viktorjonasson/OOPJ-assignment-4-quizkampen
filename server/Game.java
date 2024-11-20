2cdeb93e7b1ff6b53c920d7f9f54a985f5871882
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2024-11-20 18:07:38 +0100

    GameHandler: ny metod checkAnswer(String answer) (Fråga angående att samma metod verkar ligga i game)

--- server/Game.java
+++ server/Game.java
@@ -19,6 +19,8 @@ public class Game {
         curentRound++;
     }
 
+    //Nu har vi råkat lägga denna metod på två ställen (vi kanske haft två tickets?
+    //Ska den vara här eller i gameHandler? /Ylva
     public static boolean checkAnswer(String clientAnswer, String correctAnswer)
     {
 
