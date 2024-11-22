9311a0212a45bf0df897952d7063938d245cfe9b
viktorjonasson <viktorjonasson@gmail.com>
2024-11-22 11:50:30 +0100

    added functionality to split a category set into a question set, and saved it to an ArrayList with String[].

--- client/GameLogic.java
+++ client/GameLogic.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 
 public class GameLogic {
@@ -10,8 +11,13 @@ public class GameLogic {
 
     public void loadQuestionSet(String serverResponse) {
         System.out.println(serverResponse);
-        String[] questionSet = serverResponse.substring(1, serverResponse.length() - 1).split(",\\s*");
-        questions.add(questionSet);
+        String[] categorySet = serverResponse.substring(1, serverResponse.length() - 1).split("\\|");
+
+        for (String category : categorySet) {
+            String[] questionSet = category.trim().replaceAll("^,\\s*|\\s*,$", "").split(",\\s*");
+            System.out.println(Arrays.toString(questionSet));
+            questions.add(questionSet);
+        }
     }
 
     public Optional<String[]> getNextQuestion() {
--- server/DataBase.java
+++ server/DataBase.java
@@ -6,15 +6,17 @@ public class DataBase {
                 "Q1 Option 2",
                 "Q1 Option 3",
                 "Q1 Option 4",
+                "|",
                 "Question 2",
                 "Q2 Option 1",
                 "Q2 Option 2",
                 "Q2 Option 3",
                 "Q2 Option 4",
+                "|",
                 "Question 3",
                 "Q3 Option 1",
                 "Q3 Option 2",
                 "Q3 Option 3",
-                "Q3 Option 4",};
+                "Q3 Option 4"};
     }
 }
