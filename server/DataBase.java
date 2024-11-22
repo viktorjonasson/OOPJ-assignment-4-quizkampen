0e6f74b0efae846701c8ca4277950fba909ecc85
viktorjonasson <viktorjonasson@gmail.com>
2024-11-22 10:23:38 +0100

    expanded DataBase to 3 questions and 4 options to each

--- server/DataBase.java
+++ server/DataBase.java
@@ -1,10 +1,20 @@
 public class DataBase {
     //develop
     public String[] getQuestionSet() {
-        return new String[]{"Question",
-                "Option 1",
-                "Option 2",
-                "Option 3",
-                "Option 4"};
+        return new String[]{"Question 1",
+                "Q1 Option 1",
+                "Q1 Option 2",
+                "Q1 Option 3",
+                "Q1 Option 4",
+                "Question 2",
+                "Q2 Option 1",
+                "Q2 Option 2",
+                "Q2 Option 3",
+                "Q2 Option 4",
+                "Question 3",
+                "Q3 Option 1",
+                "Q3 Option 2",
+                "Q3 Option 3",
+                "Q3 Option 4",};
     }
 }
