c6824d1f15e096d107aa74dddc5c881e2916f4dd
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2024-11-22 10:26:43 +0100

    DataBase: QuestionSet-arrayen innehåller 3 frågor med fyra svarsalternativ vardera.

--- server/DataBase.java
+++ server/DataBase.java
@@ -1,10 +1,20 @@
 public class DataBase {
-    //develop
+
     public String[] getQuestionSet() {
-        return new String[]{"Question",
-                "Option 1",
-                "Option 2",
-                "Option 3",
-                "Option 4"};
+        return new String[]{"Question 1",
+                "Option 1.1",
+                "Option 1.2",
+                "Option 1.3",
+                "Option 1.4",
+                "Question 2",
+                "Option 2.1",
+                "Option 2.2",
+                "Option 2.3",
+                "Option 2.4",
+                "Question 3",
+                "Option 3.1",
+                "Option 3.2",
+                "Option 3.3",
+                "Option 3.4"};
     }
 }
