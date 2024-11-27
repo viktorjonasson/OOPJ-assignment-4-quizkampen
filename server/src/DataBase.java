40da127ce7a9b742921f760c416100fe3a5e2baa
Vasahlin <Vasahlin@gmail.com>
2024-11-28 00:41:14 +0100

    Changed variable name for clarity

--- server/src/DataBase.java
+++ server/src/DataBase.java
@@ -5,7 +5,7 @@ public class DataBase {
     private final Set<Integer> usedCategories = new HashSet<>();
     private final Random random = new Random();
     private int amountOfQuestions = 0;
-    private final String API_URL = "https://opentdb.com/api.php?amount=" + amountOfQuestions + "&difficulty=medium";
+    private final String API_URL_QUESTIONS = "https://opentdb.com/api.php?amount=" + amountOfQuestions + "&difficulty=medium";
     private record TriviaCategory(int id, String name) { }
 
     DataBase (int amountOfQuestions) {
