2387ccb8c8c9f7d8434369e0c58edd5a38f325b4
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2024-11-28 17:10:46 +0100

    TotalResultat visas för båda spelare när det ska.

--- server/src/DataBase.java
+++ server/src/DataBase.java
@@ -34,25 +34,6 @@ public class DataBase {
         this.amountOfQuestions = amountOfQuestions;
     }
 
-//    public ArrayList<String> getQuestionSetCorrect() {
-//        String category = "Category";
-//        Question question_1 = generateQuestion(category);
-//        Question question_2 = generateQuestion(category);
-//        Question question_3 = generateQuestion(category);
-//        ArrayList<String> question1Shuffled = question_1.getShuffled();
-//        question1Shuffled.add("|");
-//        ArrayList<String> question2Shuffled = question_2.getShuffled();
-//        question2Shuffled.add("|");
-//        ArrayList<String> question3Shuffled = question_3.getShuffled();
-//
-//        return new ArrayList<>() {{
-//            addAll(question1Shuffled);
-//            addAll(question2Shuffled);
-//            addAll(question3Shuffled);
-//        }};
-//    }
-
-
     public void loadQuestions(String category) throws Exception {
         int categoryId = getCategoryID(category);
         // Fetch questions from API
