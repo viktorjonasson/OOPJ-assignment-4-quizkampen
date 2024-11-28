d607446237abe387fc81752986f6326ca9a207ff
Vasahlin <Vasahlin@gmail.com>
2024-11-28 18:53:23 +0100

    Implemented invokeLater to try and avoid racing conditions

--- server/src/Question.java
+++ server/src/Question.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Question {
-    String category;
+    private final String category;
     String question;
     ArrayList<String> wrongAnswers;
     String rightAnswer;
@@ -15,30 +15,6 @@ public class Question {
         this.rightAnswer = rightAnswer;
     }
 
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public void setWrongAnswers(ArrayList<String> wrongAnswers) {
-        this.wrongAnswers = wrongAnswers;
-    }
-
-    public void setRightAnswer(String rightAnswer) {
-        this.rightAnswer = rightAnswer;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getQuestion() {
-        return category;
-    }
-
-    public String getRightAnswer() {
-        return rightAnswer;
-    }
-
     public String getShuffled() {
         ArrayList<String> shuffledQuestions = new ArrayList<>();
         for (String wrong : wrongAnswers) {
@@ -49,7 +25,6 @@ public class Question {
         shuffledQuestions.addFirst(question + "≈");
         String output = shuffledQuestions.toString();
         output = output.substring(1, output.length() - 1);
-        System.out.println(output);
         return output;
     }
 }
